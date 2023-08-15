fa96a5664d3a6a2f0a8d58472526b4a44365d70b
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-15 12:39:02 +0100

    Prevent empty as input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,27 +3,34 @@ import PropTypes from 'prop-types';
 
 const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodoItem(title);
-    setTitle('');
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('Please add item.');
+    }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add Todo..."
-        value={title}
-        onChange={handleChange}
-      />
-      <button type="button">Submit</button>
-    </form>
-
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+        />
+        <button type="button">Submit</button>
+      </form>
+      <span>{message}</span>
+    </>
   );
 };
 
