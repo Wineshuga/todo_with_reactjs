6a46233da07d489017637fc60f75307e65f37c7e
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-15 10:49:42 +0100

    Add navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const Navbar = () => {
-  console.log(useState(false));
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+    // Cleanup the event listener
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [dropdown]);
+
   return (
     <nav>
-      nav bar content
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>
+          <button type="button" onClick={() => setDropdown((prev) => !prev)}>
+            Services
+            {' '}
+            <span>&#8595;</span>
+          </button>
+          {dropdown && (
+          <ul>
+            <li>Design</li>
+            <li>Development</li>
+          </ul>
+          )}
+
+        </li>
+      </ul>
     </nav>
   );
 };
