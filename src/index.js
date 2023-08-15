b9d602efcfcdb3bd544453c0b9347a9222bdc6f6
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-15 13:20:31 +0100

    Remove Navbar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import TodoApp from './components/TodoApp';
-import Navbar from './components/Navbar';
 import './styles/app.css';
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <Navbar />
     <TodoApp />
   </React.StrictMode>,
 );
